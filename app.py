d2dbbbf86cc6a3d8f6709b91ef578b565d444d47
Artem <artem.tsurukin@icloud.com>
2025-06-08 23:51:35 +0300

    - создан новый шаблон "add_part.html" - добавлен маршрут в app.py - добавлена одна кнопка(Добавить деталь) в шапку HTML

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/add_part")
+def add_part():
+    return render_template("add_part.html")
+
+
 
 if __name__ == '__main__':
     app.run()
